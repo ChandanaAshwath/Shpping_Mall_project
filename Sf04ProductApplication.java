47941042c0f762046635ff6d78b152ef27e6d600
ChandanaAshwath <chandanachandu123def@gmail.com>
2024-10-18 17:11:33 +0530

    Add files via upload

---
+++ Sf04ProductApplication.java
@@ -0,0 +1,13 @@
+package com.tns.sf04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf04ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sf04ProductApplication.class, args);
+	}
+
+}
